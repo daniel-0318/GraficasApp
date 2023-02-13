005efae60910ac81736672451f01634af8b29505
Daniel Ramirez <dani_0318@hotmail.com>
2023-02-13 00:34:25 -0500

    344 fin de proyecto de uso de graficas

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -12,12 +13,43 @@ export class DonaHttpComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.graficasService.getUsuariosRedesSociales()
-    .subscribe(data =>{
-      console.log(data);
-      
+    // this.graficasService.getUsuariosRedesSociales()
+    // .subscribe(resp =>{
+    //   console.log(resp);
+    //   const labels=Object.keys(resp)
+    //     const datos=Object.values(resp)
+    //     this.doughnutChartData={
+    //       labels:Object.keys(resp),
+    //       datasets:[{data:Object.values(resp)}]
+    //     }
+    // });
+
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+    .subscribe(({labels, datasets }) => {
+      this.doughnutChartData={ labels, datasets }
     });
 
+  }
+
+  // Doughnut
+  public doughnutChartLabels: string[] =  []/* [ 'Dascargas', 'Ventas en tienda', 'Venta online' ] */;
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: []
+      },
+    ]
+  };
+
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
 }
